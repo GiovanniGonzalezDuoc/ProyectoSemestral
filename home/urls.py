cf0792fed4305cbcf7d40452b974efd655383f8d
Daniel Torres <danielte98@gmail.com>
2024-06-30 04:05:07 -0400

    avance header

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, comics, about_us
 
 urlpatterns = [
     path('', home, name="home"),
+    path('comics/', comics, name="comics"),
+    path('about-us/', about_us, name="about-us")
     
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
+
+def comics(request):
+    return render(request, 'home/comics.html')
+
+def about_us(request):
+    return render(request, 'home/about-us.html')
