37bf750f354a1869adb440693ee22469abba944a
Daniel Torres <danielte98@gmail.com>
2024-06-30 13:55:52 -0400

    reduccion css

--- auth_user/views.py
+++ auth_user/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 
 # Create your views here.
--- home/models.py
+++ home/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.AutoField(max_length=250, primary_key=True)
+    nombre = models.CharField(max_length=250)
+    descripcion = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Editorial(models.Model):
+    id_editorial = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=250)
+    descripcion = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Tipo(models.Model):
+    id_tipo = models.AutoField(max_length=250, primary_key=True)
+    nombre = models.CharField(max_length=250)
+    descripcion = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=250, null=True, blank=True)
+    imagen = models.BinaryField()
+    nombre_comic = models.CharField(max_length=250)
+    precio = models.CharField(max_length=250)
+    stock = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_comic
+
+
+class Compra(models.Model):
+    id_compra = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    precio_compra = models.IntegerField()
+    stock_producto = models.IntegerField()
+    nombre_producto = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.usuario.usuario} - {self.producto.nombre_comic}"
