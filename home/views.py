e91497c4827d2e1d371425dadf19c9e701ccef0b
Giovanni <gi.gonzalezm@duocuc.cl>
2024-07-02 19:59:00 -0400

    Comic Final

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect,HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import json
 from .models import Producto
 from common.carro import Carro
 
@@ -46,5 +48,34 @@ def limpiar_carro(request):
 def comprar(request):
     return render(request, 'common/compra.html')
 
+def agradecimiento(request):
+    return render(request, 'common/agradecimiento.html')
+
+
 def about_us(request):
     return render(request, 'home/about-us.html')
+
+@csrf_exempt
+def actualizar_stock(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        productos = data.get('productos', [])
+
+        for item in productos:
+            producto_id = item.get('id')
+            cantidad_comprada = item.get('cantidad')
+
+            # Obtener el producto desde la base de datos
+            producto = get_object_or_404(Producto, pk=producto_id)
+
+            # Verificar si hay suficiente stock disponible
+            if cantidad_comprada > producto.stock:
+                return HttpResponse(json.dumps({'success': False, 'error': 'Stock insuficiente'}), content_type='application/json', status=400)
+
+            # Actualizar el stock del producto
+            producto.stock -= cantidad_comprada
+            producto.save()
+
+        return HttpResponse(json.dumps({'success': True}), content_type='application/json')
+
+    return HttpResponse(status=405)
