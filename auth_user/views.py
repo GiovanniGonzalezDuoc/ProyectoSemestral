00b8f391d18915fd4fa67e24b0f940f37a4e00f1
Giovanni <gi.gonzalezm@duocuc.cl>
2024-07-02 21:10:52 -0400

    Comic Final

--- auth_user/views.py
+++ auth_user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def auth(request):
